6ab4ac5db55776c30114ca1058de2a7d46639692
Malloy <li3210361050@gmail.com>
2026-03-30 19:56:57 +0800

    chore(frontend): 引入 Tailwind CSS 并升级 typescript、vite 及配置
    - 添加 Tailwind CSS 支持
    - 升级 typescript 和 vite 到兼容版本
    - 调整 tsconfig.json 适配新 moduleResolution 等选项
    - 修复 vite.config.ts 中 Wails Vite 升级引起的兼容问题

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // 修复代码： 强行修正 Vite 6 在 Wails v2 中的热更新 (HMR) 迷路问题
+  server: {
+    hmr: {
+      host: "localhost",
+      protocol: "ws",
+    }
+  }
+  // ---------------
 })
