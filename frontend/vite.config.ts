403301c42cc38c631c47eeefc4d1f9e3da410f44
Malloy <li3210361050@gmail.com>
2026-04-05 15:17:42 +0800

    chore: 引入了 shadcn/ui 组件库同时完成了基础配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +13,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // 修复代码： 强行修正 Vite 6 在 Wails v2 中的热更新 (HMR) 迷路问题
   server: {
     hmr: {
